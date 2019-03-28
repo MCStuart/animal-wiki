f84721a031a75df721f0c34701d079b59f1315d4
Gavin Kimberlin and Stuart McKay <gavin@>
2019-03-28 14:52:13 -0700

    add select box to display cards of different animals

---
+++ js/scripts.js
@@ -0,0 +1,30 @@
+$(document).ready(function() {
+  $("form.change").submit(function(event) {
+    event.preventDefault();
+    var selection = $('#animal').val();
+    console.log(selection);
+    function myFunction(selection) {
+      if (selection === "lions") {
+        $("#lionCard").show();
+        $("#tigerCard").hide();
+        $("#bearCard").hide();
+      }
+      else if (selection === "tigers") {
+        $("#lionCard").hide();
+        $("#tigerCard").show();
+        $("#bearCard").hide();
+      }
+      else if (selection === "bears") {
+        $("#lionCard").hide();
+        $("#tigerCard").hide();
+        $("#bearCard").show();
+      }
+      else {
+        $("#lionCard").hide();
+        $("#tigerCard").hide();
+        $("#bearCard").hide();
+      }
+    };
+    myFunction(selection);
+  });
+});
